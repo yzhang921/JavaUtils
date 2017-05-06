fe1e5603de723ef0014e33751fc1b304fe8779c6
peter.zhang921 <peter.zhang921@gmail.com>
2017-05-06 10:00:48 +0800

    add json builder

---
+++ json/src/main/java/json/JSONBuilder.java
@@ -0,0 +1,44 @@
+package json;
+
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * Created by Peter on 2017/5/6.
+ */
+public class JSONBuilder extends JSONObject {
+
+    public static JSONBuilder json() {
+        return new JSONBuilder();
+    }
+
+    public static JSONBuilder json(String key, Object value) {
+        return new JSONBuilder().put(key, value);
+    }
+
+    @Override
+    public JSONBuilder put(String key, Object value) {
+        super.put(key, value);
+        return this;
+    }
+
+    public String toString(boolean prettyFormat, int spaces) {
+        String result = JSONObject.toJSONString(this, prettyFormat);
+        StringBuffer space = new StringBuffer();
+        if (prettyFormat) {
+            for (int i = 0; i < spaces; i++) {
+                space.append(" ");
+            }
+            return result.replaceAll("\\t", space.toString());
+        }
+        return result;
+    }
+
+    public String toString(boolean prettyFormat) {
+        return toString(prettyFormat, 0);
+    }
+
+    @Override
+    public String toString() {
+        return toString(true, 2);
+    }
+}
