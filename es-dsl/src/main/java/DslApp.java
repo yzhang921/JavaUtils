cb6683ee4bc72daf36edaeb1c81cfdb566a9505c
zyong <zyong@ctrip.com>
2017-06-29 09:25:21 +0800

    update termsquery example

--- es-dsl/src/main/java/DslApp.java
+++ es-dsl/src/main/java/DslApp.java
@@ -2,6 +2,9 @@ import static es.query.QueryBuilder.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 /**
  * Created by Peter on 2017/5/7.
@@ -12,7 +15,7 @@ public class DslApp {
 
     public static void main(String[] args) {
         LOG.info("Term query: \n{}", termQuery("name", "peter"));
-        LOG.info("Terms query: \n{}", termsQuery("name", "peter", "karen").put("boost", 1.0));
+        LOG.info("Terms query: \n{}", termsQuery("name", Arrays.asList("peter", "karen")).put("boost", 1.0));
         LOG.info("Range query: \n{}", rangeQuery("age", 15, 30, false, false));
         LOG.info("Range query: \n{}", rangeQuery("age", 15, 30, false, true));
         LOG.info("Range query: \n{}", rangeQuery("age", 15, null, true, false));
